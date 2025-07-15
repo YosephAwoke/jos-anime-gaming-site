c085c4098a08b01a1dceeb2debeace47e29e938d
YosephAwoke <yosephawoke8@gmail.com>
2025-07-15 18:18:02 +0300

    hero section video progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import {  useRef, useState } from "react";
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+  const handleVideoLoaded = () => {
+    setLoadedVideos((prev) => prev + 1);
+  }
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -8,7 +25,17 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div className="">
-          <div className="mask-clip-path"></div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg ">
+            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+              <video ref={nextVideoRef}
+              src={getVideoSrc(currentIndex + 1)}
+              loop
+              muted
+              id="current-video"
+              className="size-64 origin-center scale-150 object-cover object-center"
+              onLoadedData={handleVideoLoaded}
+              /></div>
+          </div>
         </div>
       </div>
     </div>
