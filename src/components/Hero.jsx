62dc0160b0d45479bfc40f5955b224563d35c9d2
YosephAwoke <yosephawoke8@gmail.com>
2025-07-16 13:47:49 +0300

    hero page completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,18 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 4;
+  const totalVideos = 3;
   const nextVideoRef = useRef(null);
+
   const handleVideoLoaded = () => {
     setLoadedVideos((prev) => prev + 1);
   }
 
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
   const handleMiniVdClick = () => {
     setHasClicked(true);
-    setCurrentIndex((prevIndex) => prevIndex + 1);
+    setCurrentIndex(upcomingVideoIndex);
   };
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
@@ -28,13 +31,46 @@ const Hero = () => {
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg ">
             <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
               <video ref={nextVideoRef}
-              src={getVideoSrc(currentIndex + 1)}
+              src={getVideoSrc(upcomingVideoIndex)}
               loop
               muted
               id="current-video"
               className="size-64 origin-center scale-150 object-cover object-center"
               onLoadedData={handleVideoLoaded}
               /></div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64  object-cover object-center "
+            onLoadedData={handleVideoLoaded}
+          />
+
+          <video
+            src={getVideoSrc(currentIndex === totalVideos  - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            id="current-video"
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoaded}
+          />
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute  left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading  text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-blue-75">
+              Explore, Play, and Connect with Your Favorite Characters
+            </p>
           </div>
         </div>
       </div>
