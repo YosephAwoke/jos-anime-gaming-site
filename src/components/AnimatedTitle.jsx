0ecb03d57cca73d6d50b5926b80c985e4accc7c6
YosephAwoke <yosephawoke8@gmail.com>
2025-08-05 16:21:20 +0300

    About section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AnimatedTitle = ({ title, containerClass }) => {
+  return (
+    <div className="animated-title ">
+      {title.split("<b />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split("</b>").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
