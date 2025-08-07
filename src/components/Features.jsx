b6c4c19134c912258f074b7e6a204b5dc32d92d5
YosephAwoke <yosephawoke8@gmail.com>
2025-08-07 11:42:59 +0300

    Features section end

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { TiLocation, TiLocationArrow } from "react-icons/ti";
+
+const BentoTilt = ({ children, className = "" }) => {
+
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef();
+  const handleMouseMove = (e) => {
+
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeX - 0.5) * 10; // Adjust the multiplier for tilt strength
+    const tiltY = (relativeY - 0.5) * -10; //
+    
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`;
+    setTransformStyle(newTransform);
+
+    // setTransformStyle('')
+  }
+
+  const handleMouseLeave = () => {
+    setTransformStyle('');
+  };
+
+
+  return <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ transform: transformStyle }}>{children}</div>;
+};
 
 const Features = () => {
   const BentoCard = ({ src, title, description }) => {
@@ -38,7 +68,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -49,6 +79,63 @@ const Features = () => {
             description=" A cross-platform metagame app, turning you activities across Web2 and Web3 games into a rewarding adventure."
             isCommingSoon
           />
+        </BentoTilt>
+
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  zig<b>m</b>a
+                </>
+              }
+              description="An anime and gaming-inspired NFT collection - the IP primed for expansion"
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive"
+            />
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64 text-black">
+                {" "}
+                M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
+              </h1>
+
+              <TiLocationArrow className="m-5 scale-[5] self-end" />
+            </div>
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              autoPlay
+              loop
+              muted
+              className="size-full object-cover object-center"
+            ></video>
+          </BentoTilt>
         </div>
       </div>
     </section>
