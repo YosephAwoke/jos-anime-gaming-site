f7207fc3555031d6821313a279d216736a13f7ad
YosephAwoke <yosephawoke8@gmail.com>
2025-08-07 12:44:15 +0300

    Story section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
       <About />
       <Features />
+      <Story />
     </main>
   )
 }
